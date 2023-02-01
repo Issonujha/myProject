fc8692b8ade778bb506290705fb674e4475ec667
MONU KUMAR <sjha9563@gmail.com>
2023-02-01 21:54:21 +0530

    Weather Application Sonu jha

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import org.json.JSONObject;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -39,33 +41,57 @@ public class MainActivity extends AppCompatActivity {
         check= (Button) findViewById(R.id.check);
     }
 
-    public void getWeather(View view){
+
+    public void getWeather(View view) {
         DownloadTask task=new DownloadTask();
-        task.execute("https://openweathermap.org/data/2.5/weather?q="+nameText.getText().toString()+"&appid=439d4b804bc8187953eb36d2a8c26a02");
+        task.execute("https://api.openweathermap.org/data/2.5/weather?q="+nameText.getText().toString()+"&appid=60b2a6ddb0cad60c7bbe83f9f7972156&units=metric");
     }
 
-    public class DownloadTask extends AsyncTask<String,Void,String>{
+    public class DownloadTask extends AsyncTask<String,Void,String> {
 
         @Override
         protected String doInBackground(String... urls) {
-            String result = "";
+//            String result = "";
+//            URL url;
+//            HttpURLConnection connection = null;
+//            try {
+//                url = new URL(urls[0]);
+//                connection = (HttpURLConnection) url.openConnection();
+//                InputStream inputStream = connection.getInputStream();
+//                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+//                int data = inputStreamReader.read();
+//
+//                while (data != -1) {
+//                    char current = (char) data;
+//                    result += current;
+//                    data=inputStreamReader.read();
+//                }
+//                return result;
+//            } catch (Exception e) {
+//                e.printStackTrace();
+//                return null;
+//            }
+            String object = "";
             URL url;
             HttpURLConnection connection = null;
             try {
-                url=new URL(urls[0]);
-                connection=(HttpURLConnection) url.openConnection();
-                InputStream inputStream=connection.getInputStream();
-                InputStreamReader inputStreamReader=new InputStreamReader(inputStream);
-                int data=inputStreamReader.read();
-                while (data != -1){
-                    char current=(char) data;
-                    result += current;
-                    data=inputStreamReader.read();
-                }return result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return null;
+                url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = connection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader((inputStream));
+                int data = inputStreamReader.read();
+                while(data!=-1) {
+                    char append = (char) data;
+                    object += append;
+                    data = inputStreamReader.read();
+                }
+                return object;
             }
+            catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return null;
         }
 
         @Override
@@ -78,14 +104,18 @@ public class MainActivity extends AppCompatActivity {
                 JSONObject wind=jsonObject.getJSONObject("wind");
                 JSONObject sys=jsonObject.getJSONObject("sys");
                 Long updatedAt = jsonObject.getLong("dt");
-                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt * 1000));
+                String updatedAtText = "Updated at: " + new SimpleDateFormat("dd/MM/yyyy hh:mm a", Locale.ENGLISH).format(new Date(updatedAt*1000));
                 String temp = main.getString("temp") + "°C";
                 String tempMin = "Min Temp: " + main.getString("temp_min") + "°C";
                 String tempMax = "Max Temp: " + main.getString("temp_max") + "°C";
                 String pressure = main.getString("pressure");
                 String humidity = main.getString("humidity");
                 Long sunrise = sys.getLong("sunrise");
+                Date d = new Date(sunrise*1000);
+                String sunr = ""+d;
+                sunr = sunr.substring(11, 19);
                 Long sunset = sys.getLong("sunset");
+                String suns = (""+new Date(sunset*1000)).substring(11,19);
                 String windSpeed = wind.getString("speed");
                 String weatherDescription = weather.getString("description");
                 String address = jsonObject.getString("name") + ", " + sys.getString("country");
@@ -95,13 +125,23 @@ public class MainActivity extends AppCompatActivity {
                 tempTextView.setText(temp);
                 mintempTextView.setText(tempMin);
                 maxtempTextview.setText(tempMax);
-                sunriseTextView.setText(String.valueOf(sunrise));
-                sunsetTextView.setText(String.valueOf(sunset));
+                sunriseTextView.setText(sunr);
+                sunsetTextView.setText(suns);
                 windTextView.setText(String.valueOf(windSpeed));
                 pressureTextView.setText(pressure);
                 humidityTextView.setText(humidity);
-
             }catch (Exception e){
+                cityTextView.setText("Not Valid");
+                updatedTextView.setText("");
+                cloudTextView.setText("");
+                tempTextView.setText("");
+                mintempTextView.setText("");
+                maxtempTextview.setText("");
+                sunriseTextView.setText("");
+                sunsetTextView.setText("");
+                windTextView.setText("");
+                pressureTextView.setText("");
+                humidityTextView.setText("");
                 e.printStackTrace();
             }
         }
